f5b5c4eb4248f9abc0d9c5c11575bfdb89587dd6
Sahil <ghanghas.s@husky.neu.edu>
2017-11-12 18:47:04 -0500

    phaseC added

---
+++ PhaseC/src/Set.java
@@ -0,0 +1,25 @@
+/**
+* A generic set
+*/
+interface Set<T> {
+	/**
+	* Add an element
+	*/
+	void add(T t1); 
+	/**
+	* Check if an element occurs in the set
+	*/
+	boolean IsMember(T t); 
+	/**
+	* Remove an element
+	*/
+	void remove(T t); 
+	/**
+	* Return the number of arguments in the set
+	*/
+	int size(); 
+	/**
+	* Return an iterator over the set
+	*/
+	Iterator<T> iterator();
+}
