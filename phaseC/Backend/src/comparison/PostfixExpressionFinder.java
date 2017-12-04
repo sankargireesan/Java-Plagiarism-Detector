600d15bab06f1e13fa152e8ed7d016d88f006085
ghanghas <ghanghas.s@husky.neu.edu>
2017-12-04 16:05:13 -0500

    Update PostfixExpressionFinder.java

--- phaseC/Backend/src/comparison/PostfixExpressionFinder.java
+++ phaseC/Backend/src/comparison/PostfixExpressionFinder.java
@@ -8,14 +8,21 @@ import java.util.List;
 import org.eclipse.jdt.core.dom.*;
 
 public class PostfixExpressionFinder extends ASTVisitor{
-private final List <PostfixExpression> expressions = new ArrayList <> ();
+	private final List <PostfixExpression> expressions = new ArrayList <> ();
 	
+	/*
+	 *	Returns a list of Prefix expression for the given AST node 
+	 */
 	public static List<PostfixExpression> perform(ASTNode node) {
 		PostfixExpressionFinder finder = new PostfixExpressionFinder();
 	    node.accept(finder);
 	    return finder.getexpressions();
 	}
-	
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.PostfixExpression)
+	 */
+	@Override
 	 public boolean visit (final PostfixExpression var) {
 	    expressions.add (var);
 	    return super.visit(var);
@@ -24,7 +31,7 @@ private final List <PostfixExpression> expressions = new ArrayList <> ();
 	  /**
 	   * @return an immutable list view of the postfix expressions discovered by this visitor
 	   */
-	  public List<PostfixExpression> getexpressions() {
+	  private List<PostfixExpression> getexpressions() {
 	    return expressions;
 	  }
 }
