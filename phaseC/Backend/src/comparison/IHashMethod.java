5264e03f9f5d44bfcc3853bf39fff8362f36e35e
ghanghas <ghanghas.s@husky.neu.edu>
2017-12-04 02:12:23 -0500

    Update IHashMethod.java

--- phaseC/Backend/src/comparison/IHashMethod.java
+++ phaseC/Backend/src/comparison/IHashMethod.java
@@ -1,10 +1,20 @@
 package comparison;
 
+import java.util.HashMap;
 import java.util.List;
 
 import org.eclipse.jdt.core.dom.MethodDeclaration;
-
+/*
+ * Interface to implement hash comparison method
+ */
 public interface IHashMethod {
-
+	
+	/*
+	 * Performs the hashComparison on list of methods
+	 */
 	public void hashComparison(List<MethodDeclaration> methodList1, List<MethodDeclaration> methodList2);
+	/*
+	 * Returns the hash map with similarity percentage and number of expressions in each method
+	 */
+	public HashMap <String, List<Float>> getComparisonList();
 }
