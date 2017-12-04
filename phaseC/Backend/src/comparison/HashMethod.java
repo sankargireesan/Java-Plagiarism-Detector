35ac5ef08de6f9d93acc8e3e63d7ef51abccc6fd
ghanghas <ghanghas.s@husky.neu.edu>
2017-12-04 02:11:36 -0500

    Update HashMethod.java

--- phaseC/Backend/src/comparison/HashMethod.java
+++ phaseC/Backend/src/comparison/HashMethod.java
@@ -1,16 +1,21 @@
 package comparison;
-
+/*
+ * This class performs hash comparison
+ */
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import org.eclipse.jdt.core.dom.InfixExpression;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
+import org.eclipse.jdt.core.dom.PostfixExpression;
+import org.eclipse.jdt.core.dom.PrefixExpression;
 
-import org.eclipse.jdt.core.dom.*;
 
 public class HashMethod implements IHashMethod{
 	
-	private static HashMap <String, Integer> hashvalueList1 = new HashMap<> (); // list of hash values for each method : use hashmap instead of list
-	private static HashMap <String, Integer> hashvalueList2 = new HashMap<> (); // list of hash values for each method : use hashmap instead of list
-	
+	private static HashMap <String, List<Integer>> hashvalueList1 = new HashMap<> ();
+	private static HashMap <String, List<Integer>> hashvalueList2 = new HashMap<> ();
+	private static HashMap <String, List<Float>> comparisonList = new HashMap<> ();
 	
 	/*
 	 * Hash Function
@@ -20,9 +25,9 @@ public class HashMethod implements IHashMethod{
         hashVal %= 10;
         if (hashVal < 0)
             hashVal += 10;
-        return primeSize - hashVal % primeSize;
-		
+        return primeSize - hashVal % primeSize;	
 	}
+	
 	/*
 	 *  Function to get prime number less than table size for CalHashValue function 
 	 */
@@ -90,7 +95,25 @@ public class HashMethod implements IHashMethod{
 		
 		return hashvalue;
 	}
+	/*
+	 * Returns the number of expressions in a method
+	 */
+	private int numberOfExpressions(MethodDeclaration method) {
+		List <InfixExpression> infixList = new ArrayList <> ();
+		List <PrefixExpression> prefixList = new ArrayList <> ();
+		List <PostfixExpression> postfixList = new ArrayList <> ();
+		
+		int size = 0;
+		infixList.addAll(InfixExpressionFinder.perform(method));
+		postfixList.addAll(PostfixExpressionFinder.perform(method));
+		prefixList.addAll(PrefixExpressionFinder.perform(method));
+		size = infixList.size() + postfixList.size() + prefixList.size();
+		return size;
+	}
 	
+	/*
+	 * Returns the hash value for expressions of the given method
+	 */
 	private int expressionHash(MethodDeclaration method) {
 		List <InfixExpression> infixList = new ArrayList <> ();
 		List <PrefixExpression> prefixList = new ArrayList <> ();
@@ -104,7 +127,9 @@ public class HashMethod implements IHashMethod{
 		return temp;
 	}
 	
-	
+	/*
+	 *  Returns a list of parameters of a method
+	 */
 	private List<String> getParameters (MethodDeclaration m) {
 		List<String> sb = new ArrayList<String>();
 		String s;
@@ -117,6 +142,9 @@ public class HashMethod implements IHashMethod{
 		return sb;
 	}
 	
+	/*
+	 * Returns the hash value of parameters of a method
+	 */
 	private int parameterHash (MethodDeclaration m) {
 		List<String> para = getParameters(m);
 		int p = 0;
@@ -126,41 +154,72 @@ public class HashMethod implements IHashMethod{
 		return p;
 	}
 	
-	public void hashComparison(List<MethodDeclaration> methodList1, List<MethodDeclaration> methodList2) {
+	/*
+	 * Calculates the percentage of two integers
+	 */
+	private float percentage (int number1, int number2) {
+		return ((float)Math.min(number1, number2)/(float)Math.max(number1, number2))*100;
+	}
+	
+	/*
+	 * Returns percentage of similarity
+	 */
+	private void percentageComparison (HashMap <String, List<Integer>> hashvalueList1, HashMap <String, List<Integer>> hashvalueList2) {
 		
-		/*
-		 * calculating hash value for first method list
-		 */
+		for (String str1 : hashvalueList1.keySet()) {
+			for (String str2 : hashvalueList2.keySet()) {
+				List<Float> list = new ArrayList<Float>();
+				float percent = percentage(hashvalueList1.get(str1).get(0), hashvalueList2.get(str2).get(0));
+				float expressPercent = percentage( hashvalueList1.get(str1).get(1), hashvalueList2.get(str2).get(1));
+				if(percent >=80.0 && expressPercent >= 90.0) {
+					list.add(percent);
+					list.add((float)hashvalueList1.get(str1).get(1));
+					list.add((float)hashvalueList2.get(str2).get(1));
+					comparisonList.put(str1+" : "+str2, list);
+				}
+			}
+		}
+	}
+	
+	
+	/*
+	 * Performs the hashComparison on list of methods
+	 */
+	@Override
+	public void hashComparison(List<MethodDeclaration> methodList1, List<MethodDeclaration> methodList2) {			
+		
 		for (MethodDeclaration n1: methodList1) {
-			
+			List<Integer> list = new ArrayList<Integer>();
+			int n = numberOfExpressions(n1);
 			int rT = CalHashValue(n1.getReturnType2().toString());
 			int eH = expressionHash(n1);
 			int pH = parameterHash(n1);
-			hashvalueList1.put(n1.getName().toString(),(rT + eH + pH ));
+			list.add(rT+eH+pH);
+			list.add(n);
+			hashvalueList1.put( n1.getName().toString(),list);
 		}
-//		System.out.println(methodList1.size());
-//		for (Integer m : hashvalueList1.values()) {
-//			System.out.println(m);
-//		}
-		/*
-		 * calculating HashValue for second method list
-		 */
+		
+		
+		//calculating HashValue for second method list
 		for (MethodDeclaration n2: methodList2) {
+			List<Integer> list2 = new ArrayList<Integer>();
+			int n = numberOfExpressions(n2);
 			int rT = CalHashValue(n2.getReturnType2().toString());
 			int eH = expressionHash(n2);
 			int pH = parameterHash(n2);
-			hashvalueList2.put(n2.getName().toString(), rT + eH + pH );
+			list2.add(rT+eH+pH);
+			list2.add(n);
+			hashvalueList2.put( n2.getName().toString(), list2 );
 		}
-//		for (Integer m : hashvalueList2.values()) {
-//			System.out.println(m);
-//		}
+		// calculating the percentage
+		percentageComparison(hashvalueList1,hashvalueList2);
 	}
 	
-	public static HashMap <String,Integer> getHashValueList1() {
-		return hashvalueList1;
-	}
-	
-	public static HashMap <String,Integer> getHashValueList2() {
-		return hashvalueList2;
+	/*
+	 * Returns the hash map with similarity percentage and number of expressions in each method
+	 */
+	@Override
+	public HashMap <String, List<Float>> getComparisonList() {
+		return comparisonList;
 	}
 }
