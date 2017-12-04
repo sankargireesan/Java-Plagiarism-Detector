5249ffb0c52e98f675de5cfa6fa75052c02ab609
ghanghas <ghanghas.s@husky.neu.edu>
2017-12-04 16:04:25 -0500

    Update MethodDeclarationFinder.java

--- phaseC/Backend/src/comparison/MethodDeclarationFinder.java
+++ phaseC/Backend/src/comparison/MethodDeclarationFinder.java
@@ -1,5 +1,6 @@
 package comparison;
 
+
 import java.util.ArrayList;
 import java.util.List;
 import org.eclipse.jdt.core.dom.ASTNode;
@@ -8,13 +9,18 @@ import org.eclipse.jdt.core.dom.MethodDeclaration;
 
 public class MethodDeclarationFinder extends ASTVisitor {
 	  private final List <MethodDeclaration> methods = new ArrayList <> ();
-
+	  /*
+	   * Returns a list of Prefix expression for the given AST node 
+	   */
 	  public List<MethodDeclaration> perform(ASTNode node) {
 	      MethodDeclarationFinder finder = new MethodDeclarationFinder();
 	      node.accept(finder);
 	      return finder.getMethods();
 	  }
-
+	  /*
+	   * (non-Javadoc)
+	   * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.MethodDeclaration)
+	   */
 	  @Override
 	  public boolean visit (final MethodDeclaration method) {
 	    methods.add (method);
@@ -24,7 +30,7 @@ public class MethodDeclarationFinder extends ASTVisitor {
 	  /**
 	   * @return an immutable list view of the methods discovered by this visitor
 	   */
-	  public List <MethodDeclaration> getMethods() {
+	  private List <MethodDeclaration> getMethods() {
 	    return methods;
 	  }
 	}
