60d27e6276813600c4ab6fe59603dcf48b861973
ghanghas <ghanghas.s@husky.neu.edu>
2017-12-04 16:06:07 -0500

    Update PrefixExpressionFinder.java

--- phaseC/Backend/src/comparison/PrefixExpressionFinder.java
+++ phaseC/Backend/src/comparison/PrefixExpressionFinder.java
@@ -8,14 +8,20 @@ import java.util.List;
 import org.eclipse.jdt.core.dom.*;
 
 public class PrefixExpressionFinder extends ASTVisitor{
-private final List <PrefixExpression> expressions = new ArrayList <> ();
-	
+	private final List <PrefixExpression> expressions = new ArrayList <> ();
+	/*
+	 *	Returns a list of Prefix expression for the given AST node 
+	 */
 	public static List<PrefixExpression> perform(ASTNode node) {
 		PrefixExpressionFinder finder = new PrefixExpressionFinder();
 	    node.accept(finder);
 	    return finder.getexpressions();
 	}
-	
+	/*
+	 * (non-Javadoc)
+	 * @see org.eclipse.jdt.core.dom.ASTVisitor#visit(org.eclipse.jdt.core.dom.PrefixExpression)
+	 */
+	@Override
 	 public boolean visit (final PrefixExpression var) {
 	    expressions.add (var);
 	    return super.visit(var);
@@ -24,7 +30,7 @@ private final List <PrefixExpression> expressions = new ArrayList <> ();
 	  /**
 	   * @return an immutable list view of the prefix expressions discovered by this visitor
 	   */
-	  public List<PrefixExpression> getexpressions() {
+	  private List<PrefixExpression> getexpressions() {
 	    return expressions;
 	  }
 }
